7953c81ca05a1db711e14255e466d82dbf664681
Mario Franco <mario.franco@wdev.int>
2023-02-22 15:21:42 -0300

    desafio 1 origamid

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,7 +1,46 @@
 import React from "react";
 
+
+const luana = {
+  cliente: 'Luana',
+  idade: 31,
+  compras: [
+    {nome: 'Notebook', preco: 'R$ 2500'},
+    {nome: 'Geladeira', preco: 'R$ 3000'},
+    {nome: 'SmartPhone', preco: 'R$ 1500'},
+  ],
+  situacao: true
+}
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    {nome: 'Notebook', preco: 'R$ 2500'},
+    {nome: 'Geladeira', preco: 'R$ 3000'},
+    {nome: 'SmartPhone', preco: 'R$ 1500'},
+    {nome: 'Guitarra', preco: 'R$ 3500'}
+  ],
+  situacao: false
+}
 const App = () => {
-  return <div>Meu app</div>
+  
+  const dados = mario;
+
+  const total = dados.compras.map(item => Number(item.preco.replace('R$',""))).reduce((a,b) => a+b)
+  
+
+  return <div>
+    <p>Nome: {dados.cliente}</p>
+    <p>Idade: {dados.idade}</p>
+    <p>
+      Situação:<span style={{color: dados.situacao ? 'green' : 'red'}}>{dados.situacao ? 'Ativa' : 'Inativa'}</span> 
+    
+    </p>
+    <p>Total: R${total}</p>
+    {total > 10000 && <p>Você está gastando muito</p>}
+  </div> 
+          
 }
 
 export default App
