590e6865b5253f2baeb0a39295e0abf78b74744a
Mario Franco <mario.franco@wdev.int>
2023-02-24 10:33:09 -0300

    hooks

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,46 +1,18 @@
 import React from "react";
 
 
-const luana = {
-  cliente: 'Luana',
-  idade: 31,
-  compras: [
-    {nome: 'Notebook', preco: 'R$ 2500'},
-    {nome: 'Geladeira', preco: 'R$ 3000'},
-    {nome: 'SmartPhone', preco: 'R$ 1500'},
-  ],
-  situacao: true
-}
-
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    {nome: 'Notebook', preco: 'R$ 2500'},
-    {nome: 'Geladeira', preco: 'R$ 3000'},
-    {nome: 'SmartPhone', preco: 'R$ 1500'},
-    {nome: 'Guitarra', preco: 'R$ 3500'}
-  ],
-  situacao: false
-}
 const App = () => {
-  
-  const dados = mario;
+  const ativoHook= React.useState(false)
+  const ativoValor = ativoHook[0];
+  const atualizaValor = ativoHook[1];
+  console.log(ativoValor)
 
-  const total = dados.compras.map(item => Number(item.preco.replace('R$',""))).reduce((a,b) => a+b)
-  
-
-  return <div>
-    <p>Nome: {dados.cliente}</p>
-    <p>Idade: {dados.idade}</p>
-    <p>
-      Situação:<span style={{color: dados.situacao ? 'green' : 'red'}}>{dados.situacao ? 'Ativa' : 'Inativa'}</span> 
-    
-    </p>
-    <p>Total: R${total}</p>
-    {total > 10000 && <p>Você está gastando muito</p>}
-  </div> 
-          
+  function handleClick() {
+    atualizaValor(!ativoValor)
+  }
+  return (
+    <button onClick={handleClick}>{ativoValor ? 'Ativo' : 'Inativo'}</button>
+  )
 }
 
 export default App
